4a4eb7d3cf9a519f7a495bca510f712b59ee63b4
Abhi932321 <akbgs999@gmail.com>
2026-05-05 18:26:34 +0530

    Fixed code structure and indentation

--- simulation.py
+++ simulation.py
@@ -1,7 +1,16 @@
 # Priority Inversion Simulation Project
+
 class Process:
     def __init__(self, pid, priority):
         self.pid = pid
         self.priority = priority
-        def scheduler(processes):
-            return max(processes, key=lambda x: x.priority)
+
+
+def scheduler(processes):
+    return max(processes, key=lambda x: x.priority)
+
+
+class Resource:
+    def __init__(self):
+        self.locked = False
+        self.holder = None
