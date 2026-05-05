2b0b8d87d32cf1e009d7538c142b982a90f07781
Abhi932321 <akbgs999@gmail.com>
2026-05-05 18:24:01 +0530

    Added basic scheduler logic

--- simulation.py
+++ simulation.py
@@ -3,3 +3,5 @@ class Process:
     def __init__(self, pid, priority):
         self.pid = pid
         self.priority = priority
+        def scheduler(processes):
+            return max(processes, key=lambda x: x.priority)
